ec1d4f044e83d70dd14e6e27de43891cd23dc8d0
Art Simon <simart@gmail.com>
2016-06-06 12:13:39 -0700

    Update KA_Offline.js

--- KA_Offline.js
+++ KA_Offline.js
@@ -1,10 +1,7 @@
 
-var sketchProc=function(processingInstance){ 
-  with (processingInstance){
+
     size(400, 400); 
     frameRate(30);
     draw = function() {
       ellipse(150,50,50,50);
     }
-  }
-};
